077eaaf3e6a6a2ca7692c4fb3bdc5d91a000ae50
Johnny Street <johnnystreet@gmail.com>
2023-12-03 15:55:59 -0500

    removed debug line in run.bat

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 
 setlocal
 
-set CUDA_HOME=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.1
+rem set CUDA_HOME=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.1
 
 if not exist "venv\" (    
     echo Creating virtual environment...
