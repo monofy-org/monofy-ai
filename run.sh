c3cc1aa951330de0330cce38c2f427a853df5101
Johnny Street <johnnystreet@gmail.com>
2024-01-03 20:43:41 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,7 +18,7 @@ if [ ! -d "venv" ]; then
     python3 -m venv venv
     source venv/bin/activate
     python3 -m pip install --upgrade pip
-    python3 -m pip install -r requirements.txt -r requirements-linux.txt --extra-index-url $TORCH_INDEX_URL
+    python3 -m pip install -r requirements-cuda.txt -r requirements.txt -r requirements-linux.txt --extra-index-url $TORCH_INDEX_URL
     # git clone https://github.com/zhan-xu/RigNet.git modules/RigNet
     if [ "$USE_CUDA" = "False" ]; then
         ./venv/bin/python3 run.py "$@"
