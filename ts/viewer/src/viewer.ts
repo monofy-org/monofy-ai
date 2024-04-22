43587d03476f639176c34704aeac92d2f2927f26
Johnny Street <johnnystreet@gmail.com>
2024-04-22 09:28:23 -0400

    viewer: added right-click context menu, basic primitives

--- ts/viewer/src/viewer.ts
+++ ts/viewer/src/viewer.ts
@@ -19,6 +19,7 @@ import "@babylonjs/core/Helpers/sceneHelpers";
 import { ScaleGizmo } from "@babylonjs/core/Gizmos/scaleGizmo";
 import { BoundingBoxGizmo } from "@babylonjs/core/Gizmos/boundingBoxGizmo";
 import { GroundMesh } from "@babylonjs/core/Meshes/groundMesh";
+import { ContextMenu } from "./elements/ContextMenu";
 //import { Inspector } from "@babylonjs/inspector";
 
 const win = window as any;
@@ -30,6 +31,7 @@ export class Viewer {
   private readonly _camera: ArcRotateCamera;
   private readonly _cursor: Mesh;
   private readonly _defaultMaterial;
+  private readonly _contextMenu: ContextMenu;
   private _selectedMesh: Nullable<AbstractMesh> = null;
   public readonly gizmoManager: GizmoManager;
   public readonly cursorPosition: Vector3 = Vector3.Zero();
@@ -50,6 +52,44 @@ export class Viewer {
       this._canvas.width = win.innerWidth;
       this._canvas.height = win.innerHeight;
     });
+
+    this._contextMenu = new ContextMenu(document.body, this._canvas);
+    const primitiveMenu = new ContextMenu();
+
+    primitiveMenu.addItem("Box", () => {
+      this.addPrimitive(
+        "box",
+        this.cursorPosition,
+        Vector3.Zero(),
+        new Vector3(1, 1, 1)
+      );
+    });
+    primitiveMenu.addItem("Sphere", () => {
+      this.addPrimitive(
+        "sphere",
+        this.cursorPosition,
+        Vector3.Zero(),
+        new Vector3(1, 1, 1)
+      );
+    });
+    primitiveMenu.addItem("Cylinder", () => {
+      this.addPrimitive(
+        "cylinder",
+        this.cursorPosition,
+        Vector3.Zero(),
+        new Vector3(1, 1, 1)
+      );
+    });
+    primitiveMenu.addItem("Torus", () => {
+      this.addPrimitive(
+        "torus",
+        this.cursorPosition,
+        Vector3.Zero(),
+        new Vector3(1, 1, 1)
+      );
+    });
+
+    this._contextMenu.addSubmenu("Add Primitive", primitiveMenu);
 
     this._engine = new Engine(this._canvas, true);
     this._scene = new Scene(this._engine);
@@ -79,10 +119,10 @@ export class Viewer {
     this._defaultMaterial.specularColor = new Color3(0, 0, 0); // No shininess
     this._camera = new ArcRotateCamera(
       "camera",
-      0,
-      0,
+      Math.PI / 2,
+      Math.PI / 2,
       10,
-      Vector3.Zero(),
+      new Vector3(0, 1.65, 0),
       this._scene
     );
     this._camera.attachControl(this._canvas, true);
@@ -133,16 +173,13 @@ export class Viewer {
       throw new Error("Gizmo manager already created");
     }
 
-    let clickedMesh: Nullable<AbstractMesh> = null;
-    let clickedTime = 0;
-
     win.addEventListener("keydown", (event: KeyboardEvent) => {
       if (event.key === "Delete") {
         if (this._selectedMesh) {
           gizmoManager.attachToMesh(null);
           this._selectedMesh.dispose();
         }
-      } else if (event.key === "e") {
+      } else if (event.key === "e" && this._selectedMesh !== this._cursor) {
         if (this._selectedMesh) {
           gizmoManager.rotationGizmoEnabled = true;
           gizmoManager.positionGizmoEnabled = false;
@@ -156,65 +193,67 @@ export class Viewer {
           gizmoManager.scaleGizmoEnabled = false;
           gizmoManager.boundingBoxGizmoEnabled = false;
         }
-      } else if (event.key === "q") {
+      } else if (event.key === "q" && this._selectedMesh !== this._cursor) {
         if (this._selectedMesh) {
           gizmoManager.positionGizmoEnabled = true;
           gizmoManager.rotationGizmoEnabled = true;
+          gizmoManager.boundingBoxGizmoEnabled = true;
           gizmoManager.scaleGizmoEnabled = false;
-          gizmoManager.boundingBoxGizmoEnabled = false;
         }
-      } else if (event.key === "s") {
+      } else if (event.key === "s" && this._selectedMesh !== this._cursor) {
         if (this._selectedMesh) {
           gizmoManager.positionGizmoEnabled = false;
           gizmoManager.rotationGizmoEnabled = false;
           gizmoManager.scaleGizmoEnabled = true;
           gizmoManager.boundingBoxGizmoEnabled = false;
         }
-      } else if (event.key === "r") {
+      } else if (event.key === "r" && this._selectedMesh !== this._cursor) {
         if (this._selectedMesh) {
           gizmoManager.boundingBoxGizmoEnabled = true;
           gizmoManager.positionGizmoEnabled = false;
           gizmoManager.rotationGizmoEnabled = false;
           gizmoManager.scaleGizmoEnabled = false;
         }
+      } else if (event.key === "c") {
+        this._placeCursor();
       } else if (event.key == "Escape") {
-        this.deselectAll();
+        if (this._contextMenu.isVisible()) {
+          this._contextMenu.hide();
+        } else {
+          this.deselectAll();
+        }
       }
     });
 
     this._scene.onPointerObservable.add((eventData) => {
-      if (eventData.type === PointerEventTypes.POINTERDOWN) {
-        clickedTime = new Date().getTime();
-        if (eventData.pickInfo?.hit) {
-          clickedMesh = eventData.pickInfo.pickedMesh;
-        }
-      } else if (eventData.type === PointerEventTypes.POINTERUP) {
-        if (new Date().getTime() - clickedTime > 200) {
-          return;
-        }
-        if (
-          eventData.pickInfo?.hit &&
-          eventData.pickInfo.pickedMesh === clickedMesh
-        ) {
-          console.log(eventData.pickInfo.pickedMesh?.name);
+      if (eventData.event.button === 2) {
+        this._placeCursor();
+        return;
+      }
 
-          // Store the selected mesh
-          this._selectedMesh = eventData.pickInfo.pickedMesh;
-          // this._scene.debugLayer.select(selectedMesh);
+      if (eventData.event.button !== 0) {
+        return;
+      }
 
-          gizmoManager.positionGizmoEnabled = true;
-          gizmoManager.rotationGizmoEnabled = false;
-          gizmoManager.scaleGizmoEnabled = false;
-          gizmoManager.boundingBoxGizmoEnabled = false;
-          gizmoManager.attachToMesh(this._selectedMesh);
+      if (eventData.type === PointerEventTypes.POINTERDOUBLETAP) {
+        if (eventData.pickInfo?.hit && eventData.pickInfo.pickedMesh) {
+          this.selectObject(eventData.pickInfo.pickedMesh);
         } else {
-          // Deselect the mesh
+          this.deselectAll();
+        }
+      } else if (eventData.type === PointerEventTypes.POINTERDOWN) {
+        if (eventData.pickInfo?.hit && eventData.pickInfo.pickedMesh) {
+          if (eventData.pickInfo.pickedMesh !== this._selectedMesh) {
+            this.deselectAll();
+          }
+        } else {
           this.deselectAll();
         }
       }
     });
 
     const gizmoManager = new GizmoManager(this._scene);
+    gizmoManager.usePointerToAttachGizmos = false;
     gizmoManager.gizmos.positionGizmo = new PositionGizmo();
     gizmoManager.gizmos.rotationGizmo = new RotationGizmo();
     gizmoManager.gizmos.scaleGizmo = new ScaleGizmo();
@@ -227,6 +266,38 @@ export class Viewer {
     return gizmoManager;
   }
 
+  private _placeCursor() {
+    const pickResult = this._scene.pick(
+      this._scene.pointerX,
+      this._scene.pointerY
+    );
+    if (pickResult.hit && pickResult.pickedPoint) {
+      this.cursorPosition.copyFrom(pickResult.pickedPoint);
+      this._cursor.position.copyFrom(this.cursorPosition);
+      this._cursor.isVisible = true;
+      this.selectObject(this._cursor);
+    }
+  }
+
+  public selectObject(mesh: AbstractMesh) {
+    if (mesh !== this._cursor) {
+      this._cursor.isVisible = false;
+    }
+
+    if (mesh === this._selectedMesh) {
+      return;
+    }
+
+    this._selectedMesh = mesh;
+    // this._scene.debugLayer.select(selectedMesh);
+
+    this.gizmoManager.positionGizmoEnabled = true;
+    this.gizmoManager.rotationGizmoEnabled = false;
+    this.gizmoManager.scaleGizmoEnabled = false;
+    this.gizmoManager.boundingBoxGizmoEnabled = false;
+    this.gizmoManager.attachToMesh(this._selectedMesh);
+  }
+
   public deselectAll(): void {
     this._selectedMesh = null;
     this.gizmoManager.attachToMesh(null);
@@ -234,14 +305,48 @@ export class Viewer {
     this.gizmoManager.rotationGizmoEnabled = false;
     this.gizmoManager.scaleGizmoEnabled = false;
     this.gizmoManager.boundingBoxGizmoEnabled = false;
+    this._cursor.isVisible = false;
+    this._contextMenu.hide();
   }
 
-  public addBox(position: Vector3, rotation: Vector3, size: Vector3) {
-    const box = MeshBuilder.CreateBox("box", { size: 1 }, this._scene);
-    box.position = position;
-    box.rotation = rotation;
-    box.scaling = size;
-    box.material = this._defaultMaterial;
+  public addPrimitive(
+    type: "box" | "sphere" | "cylinder" | "torus",
+    position: Vector3,
+    rotation: Vector3,
+    size: Vector3
+  ) {
+    let mesh: Mesh;
+    switch (type) {
+      case "box":
+        mesh = MeshBuilder.CreateBox("box", { size: 1 }, this._scene);
+        break;
+      case "sphere":
+        mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, this._scene);
+        break;
+      case "cylinder":
+        mesh = MeshBuilder.CreateCylinder(
+          "cylinder",
+          { diameterTop: 1, diameterBottom: 1, height: 1 },
+          this._scene
+        );
+        break;
+      case "torus":
+        mesh = MeshBuilder.CreateTorus(
+          "torus",
+          { diameter: 1, thickness: 0.5 },
+          this._scene
+        );
+        break;
+      default:
+        throw new Error("Invalid primitive type");
+    }
+
+    mesh.position.copyFrom(position);
+    mesh.rotation.copyFrom(rotation);
+    mesh.scaling.copyFrom(size);
+    mesh.material = this._defaultMaterial;
+
+    this.selectObject(mesh);
   }
 
   public enableXR() {
