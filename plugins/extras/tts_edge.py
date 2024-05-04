4effe72ea62012e2dece4b7d6629bda8ad98fa2e
Johnny Street <johnnystreet@gmail.com>
2024-05-04 18:03:41 -0400

    tts/edge fixes

--- plugins/extras/tts_edge.py
+++ plugins/extras/tts_edge.py
@@ -22,18 +22,9 @@ async def edge_initialize():
             logging.info(f"Voice {i}: {v['ShortName']}")
 
 
-@router.post("/tts/edge", tags=["Text-to-Speech"])
-async def tts_edge(req: TTSRequest):
-
-    result_io = await generate_speech_edge(req.text, req.voice, req.speed)
-    return StreamingResponse(
-        result_io,
-        media_type="audio/mpeg",
-        headers={"Content-Disposition": "attachment", "filename": "output.mp3"},
-    )
-
-
-async def generate_speech_edge(text: str, voice: str, speed: float = 1.0):
+async def generate_speech_edge(
+    text: str, voice: str, speed: float = 1.0, pitch: float = 1.0
+):
     if voice_manager is None:
         await edge_initialize()
     voices = voice_manager.find(ShortName=voice)
@@ -42,16 +33,41 @@ async def generate_speech_edge(text: str, voice: str, speed: float = 1.0):
         raise HTTPException(status_code=400, detail=f"Voice {voice} not found.")
 
     print(f"Using voice: {voices[0]['Name']}")
-    prefix = "+" if (speed - 1 >= 0) else ""
-    rate = f"{prefix}{round((speed - 1) * 100)}%"
-    communicate = edge_tts.Communicate(
-        process_text_for_tts(text), voices[0]["Name"], rate=rate
-    )
+
+    args = dict({
+        "text": text,
+        "voice": voices[0]["Name"],        
+    })
+
+    if speed != 1:
+        prefix = "+" if (speed - 1 >= 0) else ""
+        rate = f"{prefix}{round((speed - 1) * 100)}%"
+        args["rate"] = rate
+
+    if pitch != 1:
+        s = (pitch - 1) * 100
+        p = ("+" if s > 0 else "-") + str(s) + "%"
+        args["pitch"] = p
+
+    communicate = edge_tts.Communicate(**args)
 
     async for chunk in communicate.stream():
         if chunk["type"] == "audio":
             data: bytes = chunk["data"]
             yield data
+
+
+@router.post("/tts/edge", tags=["Text-to-Speech"])
+async def tts_edge(req: TTSRequest):
+
+    if req.voice == "female1":
+        req.voice = "en-US-AvaNeural"
+
+    return StreamingResponse(
+        generate_speech_edge(req.text, req.voice, req.speed),
+        media_type="audio/mpeg",
+        headers={"Content-Disposition": "attachment", "filename": "output.mp3"},
+    )
 
 
 @router.get("/tts/edge/voices", tags=["Text-to-Speech"])
