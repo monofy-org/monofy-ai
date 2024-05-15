d3899249e58f64400ba5b18d3c2209a1c268a8d4
Johnny Street <johnnystreet@gmail.com>
2024-05-14 20:39:01 -0400

    stable diffusion: set SD15 default size to 512x512

--- modules/filter.py
+++ modules/filter.py
@@ -7,9 +7,7 @@ from utils.text_utils import translate_emojis
 
 
 def filter_request(req: Txt2ImgRequest):
-    if req.width < 64 or req.height < 64:
-        raise HTTPException(406, "Image dimensions should be at least 64x64")
-    
+
     req.model_index = req.model_index or SD_DEFAULT_MODEL_INDEX
     model_name = SD_MODELS[req.model_index]
     is_xl = "xl" in model_name.lower()        
@@ -17,7 +15,10 @@ def filter_request(req: Txt2ImgRequest):
     if not req.width:
         req.width = 768 if is_xl else 512
     if not req.height:
-        req.height = 768 if is_xl else 512
+        req.height = 768 if is_xl else 512    
+
+    if req.width < 64 or req.height < 64:
+        raise HTTPException(406, "Image dimensions should be at least 64x64")
 
     if req.width % 64 != 0 or req.height % 64 != 0:
         req.width = req.width - (req.width % 64)
