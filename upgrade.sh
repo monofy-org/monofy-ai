0b47c9408edcd42b2b3891f91c7f2ea3744c8e26
Johnny Street <johnnystreet@gmail.com>
2024-09-19 11:59:18 -0400

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -2,7 +2,6 @@
 source venv/bin/activate
 
 git pull
-python3 -m pip install --upgrade -r requirements/requirements-wsl.txt
-python3 -m pip install --upgrade -r requirements/requirements-secondary.txt
+python3 -m pip install --upgrade -r requirements/requirements-wsl.txt -r requirements/requirements-secondary.txt
 git submodule init
 git submodule update
